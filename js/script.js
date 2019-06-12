8e831d1955722b058ec6b234e1268795d2994864
oduvanka <qcatjul@gmail.com>
2019-06-12 20:08:56 +0400

    Исправляет поиск селекторов в зависимости от открытой страницы

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 var
   link = document.querySelector(".login-link"),
   popup = document.querySelector(".modal-login"),
-  close = popup.querySelector(".modal-close"),
-  form = popup.querySelector("form");
-  login = popup.querySelector("[name=login]"),
-  password = popup.querySelector("[name=password]"),
+  close,
+  form,
+  login,
+  password,
   mapLink = document.querySelector(".contacts-button-map"),
   mapPopup = document.querySelector(".modal-map"),
-  mapClose = mapPopup.querySelector(".modal-close");
+  mapClose;
+  
+  
 
 var isStorageSupport = true;
 
@@ -21,71 +23,87 @@ catch (err) {
   isStorageSupport = false;
 }
 
-/* Добавляет событие клика по кнопке Войти */
-link.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  popup.classList.add("modal-show");
-
-  if (storage) {
-    login.value = storage;
-    password.focus();
-  }
-  else {
-    login.focus();
-  }
-});
-
-/* Добавляет обработчик клика по кнопке Закрыть в окне авторизации */
-close.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-show");
-  popup.classList.remove("modal-error");
-});
-
-/* Добавляет обработчик нажатия по клавише Esc при открытом окне авторизации */
-window.addEventListener("keydown", function(evt) {
-  if (evt.keyCode === 27) {
+if (link) {
+  /* Добавляет событие клика по кнопке Войти */
+  link.addEventListener("click", function(evt) {
     evt.preventDefault();
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show");
-      popup.classList.remove("modal-error");
+    popup.classList.add("modal-show");
+
+    if (storage) {
+      login.value = storage;
+      password.focus();
     }
-  }
-});
+    else {
+      login.focus();
+    }
+  });
+}
 
-/* Отслеживает отправку формы авторизации */
-form.addEventListener("submit", function(evt) {
-  if (!login.value || !password.value) {
+if (popup) {
+  close = popup.querySelector(".modal-close");
+  form = popup.querySelector("form");
+  login = popup.querySelector("[name=login]");
+  password = popup.querySelector("[name=password]");
+
+  /* Добавляет обработчик клика по кнопке Закрыть в окне авторизации */
+  close.addEventListener("click", function(evt) {
     evt.preventDefault();
+    popup.classList.remove("modal-show");
     popup.classList.remove("modal-error");
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-error");
-  }
-  else {
-    if (isStorageSupport) {
-      localStorage.setItem("login", login.value);
+  });
+
+  /* Добавляет обработчик нажатия по клавише Esc при открытом окне авторизации */
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
     }
-  }
-});
+  });
 
-/* Добавляет обработчик клика по кнопке Как проехать */
-mapLink.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  mapPopup.classList.add("modal-show");
-});
-
-/* Добавляет обработчик клика по кнопке Закрыть в окне с картой */
-mapClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  mapPopup.classList.remove("modal-show");
-});
-
-/* Добавляет обработчик нажатия по клавише Esc при открытом окне с картой */
-window.addEventListener("keydown", function(evt) {
-  evt.preventDefault();
-  if (evt.keyCode === 27) {
-    if (mapPopup.classList.contains("modal-show")) {
-      mapPopup.classList.remove("modal-show");
+  /* Отслеживает отправку формы авторизации */
+  form.addEventListener("submit", function(evt) {
+    if (!login.value || !password.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
     }
-  }
-});
+    else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", login.value);
+      }
+    }
+  });
+}
+
+if (mapLink) {
+  /* Добавляет обработчик клика по кнопке Как проехать */
+  mapLink.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+  });
+}
+
+if (mapPopup) {
+  mapClose = mapPopup.querySelector(".modal-close");
+
+  /* Добавляет обработчик клика по кнопке Закрыть в окне с картой */
+  mapClose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+  });
+
+  /* Добавляет обработчик нажатия по клавише Esc при открытом окне с картой */
+  window.addEventListener("keydown", function(evt) {
+    evt.preventDefault();
+    if (evt.keyCode === 27) {
+      if (mapPopup.classList.contains("modal-show")) {
+        mapPopup.classList.remove("modal-show");
+      }
+    }
+  });
+}
+
